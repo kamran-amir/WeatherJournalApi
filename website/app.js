9eee479334159c7c7a9a4e4ac48279da53ec3739
kamran-amir <amiraslanov.k>
2020-05-28 09:12:52 -0700

    added

---
+++ website/app.js
@@ -0,0 +1,90 @@
+// http://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&APPID={APIKEY}
+
+const APIKEY = 'af358d74e77edd47ddb9850e6766e548' 
+const apiBaseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+
+const fetchWeather = async function (url) {
+    let response = await fetch(url);
+    try {
+      let data = await response.json();
+      // console.log(data);
+      return data;
+    } catch (err) {
+      console.log("Err:", err)
+    }
+  }
+
+  const generate= async function() {
+    const zip = document.getElementById('inputZip').value;
+    const content = document.getElementById('feeling').value;
+    const url = `${apiBaseUrl}${zip}&APPID=${APIKEY}`;
+    
+    if (zip.length === 0 || feeling.length === 0) {
+      alert("Please fill up all values !");
+      return
+    }
+  
+    let weatherData = await fetchWeather(url); // return object
+    // console.log("weatherData=",weatherData);
+    
+    let temp = weatherData.main.temp;
+  
+    // Create a new date instance dynamically with JS
+    let d = new Date();
+    let date = d.getDate() + '.'+ (d.getMonth() + 1 )+ '.' + d.getFullYear();
+  
+    const data = {
+      date: date,
+      temp: temp,
+      content: content,
+    }
+    
+    //Post data to owr own server
+    await postData("http://localhost:8000/projectData", data);
+    
+    //Update UI
+    updateUI();  
+  }
+
+  const updateUI= async function() {
+    const dateDiv = document.getElementById('date');
+    const tempDiv = document.getElementById('temp');
+    const contentDiv = document.getElementById('content');
+    
+    //Get data from owr own server
+    let UI_Data = await getData("http://localhost:8000/projectData");
+    
+    //Updating the UI
+    dateDiv.innerText = UI_Data.date;
+    tempDiv.innerText = UI_Data.temp;
+    contentDiv.innerText = UI_Data.content;
+  }
+
+  async function postData(url,data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    return await response.json(); 
+  }
+
+  const getData= async function(url) {
+    let response = await fetch(url)
+    try {
+      let data = response.json();
+      console.log(data);
+      
+      return data;
+    } catch(err){
+      console.log(err);
+    }
+   
+  }
+  
+
+  
+const generateBtn = document.querySelector('#generate');
+generateBtn.addEventListener('click', generate);
